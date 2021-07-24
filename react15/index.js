0b562c92d46b752926ca1bff28d06a8bdcde4406
nbhaohao <nbhaohao@163.com>
2021-07-24 10:57:31 +0800

    feat: 引入 React15 应用

---
+++ react15/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import BasicMap from './src/router/index.jsx';
+import "./index.scss"
+
+ReactDOM.render((
+  <BasicMap/>
+), document.getElementById('app-react'))
