2f00a7fe43041c5163fee193d9ddeea957f0df08
nbhaohao <nbhaohao@163.com>
2021-07-26 14:26:16 +0800

    feat: vue2 子应用暴露生命周期方法

--- vue2/src/main.js
+++ vue2/src/main.js
@@ -3,7 +3,29 @@ import App from './App.vue'
 import router from './router';
 
 Vue.config.productionTip = false
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app-vue')
+
+let instance = null;
+const render = () => {
+  instance = new Vue({
+    router,
+    render: h => h(App)
+  }).$mount('#app-vue')
+}
+
+if (!window.__MICRO_WEB__) {
+  render()
+}
+
+// 开始加载结构
+export const bootstrap = () => {
+  console.log('开始加载')
+}
+
+export const mount = () => {
+  render()
+  console.log("渲染成功")
+}
+
+export const unmount = () => {
+  console.log('卸载', instance)
+}
